8a67c5d06912b995430b6600923930a80b39abd4
Kiran Malieakkal <kiranmalieakkal18@gmail.com>
2024-12-24 16:58:42 +0100

    Adding Auth0 dependencies to implement Authentication

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,14 +2,38 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { Auth0Provider } from "@auth0/auth0-react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const domain = import.meta.env.VITE_AUTH0_DOMAIN as string;
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID as string;
+
+if (!domain || !clientId) {
+  throw new Error("Auth0 environment variables are missing");
+}
 
 const queryClient = new QueryClient({});
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{
+        redirect_uri: `${window.location.origin}${
+          import.meta.env.VITE_REDIRECT_URI !== undefined
+            ? import.meta.env.VITE_REDIRECT_URI
+            : ""
+        }`,
+        audience: import.meta.env.VITE_AUDIENCE,
+      }}
+      /*  authorizationParams={{
+      redirect_uri: authConfig.redirectUri,
+    }} */
+    >
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </Auth0Provider>
   </React.StrictMode>
 );
 
